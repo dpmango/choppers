f0306f3592e90db20f1f375d123dbbd048cbfb79
Sergey Khmelevskoy <xs290@me.com>
2017-11-13 21:07:49 +0300

    homepage

--- src/js/main.js
+++ src/js/main.js
@@ -24,12 +24,37 @@ $(document).ready(function(){
     }
   }
 
+  // IE Fixes
+  function msieversion() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  if ( msieversion() ){
+    $('body').addClass('is-ie');
+  }
+
   //////////
   // COMMON
   //////////
 
   // svg support for laggy browsers
   svg4everybody();
+
+  // Viewport units buggyfill
+  window.viewportUnitsBuggyfill.init({
+    force: true,
+    hacks: window.viewportUnitsBuggyfillHacks,
+    refreshDebounceWait: 250,
+    appendToBody: true
+  });
+
 
  	// Prevent # behavior
 	$('[href="#"]').click(function(e) {
@@ -43,6 +68,35 @@ $(document).ready(function(){
             scrollTop: $(el).offset().top}, 1000);
         return false;
 	});
+
+
+  // FOOTER REVEAL
+  function revealFooter() {
+    var footer = $('[js-reveal-footer]');
+    if (footer.length > 0) {
+      var footerHeight = footer.outerHeight();
+      var maxHeight = _window.height() - footerHeight > 100;
+      if (maxHeight && !msieversion() ) {
+        $('body').css({
+          'margin-bottom': footerHeight
+        });
+        footer.css({
+          'position': 'fixed',
+          'z-index': -10
+        });
+      } else {
+        $('body').css({
+          'margin-bottom': 0
+        });
+        footer.css({
+          'position': 'static',
+          'z-index': 10
+        });
+      }
+    }
+  }
+  revealFooter();
+  _window.resized(100, revealFooter);
 
   // HEADER SCROLL
   // add .header-static for .page or body
@@ -97,44 +151,31 @@ $(document).ready(function(){
   // SLIDERS
   //////////
 
-  $('.trending__wrapper').slick({
-    autoplay: true,
+  $('.menu__slider').slick({
+    autoplay: false,
     dots: false,
-    arrows: false,
+    arrows: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true
+    vertical: false,
+    fade: true,
+    slidesToShow: 1
+  });
+
+  $('.interior__slider').slick({
+    autoplay: false,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    vertical: false,
+    fade: false,
+    slidesToShow: 1
   });
 
   //////////
   // MODALS
   //////////
-  // Custom modals
-  // $('*[data-modal]').on('click', function(){
-  //   // remove all active first
-  //   $('.modal').removeClass('opened');
-  //
-  //   // find by id
-  //   var target = $(this).data('modal');
-  //   $('#'+target).addClass('opened');
-  //
-  //   window.location.hash = target;
-  // });
-  //
-  // $('.modal__close').on('click', function(){
-  //   $(this).closest('.modal').removeClass('opened');
-  //   window.location.hash = "";
-  // });
-  //
-  // // CHECK SAVED STATE
-  // if(window.location.hash) {
-  //   var hash = window.location.hash.substring(1);
-  //   $('#'+hash).addClass('opened');
-  // }
-  //
-
 
   // Magnific Popup
   // var startWindowScroll = 0;
@@ -160,89 +201,110 @@ $(document).ready(function(){
     }
   });
 
-  // $('.popup-gallery').magnificPopup({
-	// 	delegate: 'a',
-	// 	type: 'image',
-	// 	tLoading: 'Loading image #%curr%...',
-	// 	mainClass: 'mfp-img-mobile',
-	// 	gallery: {
-	// 		enabled: true,
-	// 		navigateByImgClick: true,
-	// 		preload: [0,1]
-	// 	},
-	// 	image: {
-	// 		tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-	// 	}
-	// });
+  $('[js-popup-gallery]').magnificPopup({
+		delegate: '.insta-feed__pic',
+		type: 'image',
+		tLoading: 'Загрузка #%curr%...',
+    closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+    removalDelay: 300,
+		mainClass: 'mfp-no-margins mfp-with-zoom',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1]
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+		}
+	});
 
 
   ////////////
-  // UI
+  // SCROLLMONITOR
   ////////////
+  var monitorActive = false;
+  window.runScrollMonitor = function(){
+    setTimeout(function(){
 
-  // custom selects
-  $('.ui-select__visible').on('click', function(e){
-    var that = this
-    // hide parents
-    $(this).parent().parent().parent().find('.ui-select__visible').each(function(i,val){
-      if ( !$(val).is($(that)) ){
-        $(val).parent().removeClass('active')
+      // require
+      if ( !monitorActive ){
+        monitorActive = true;
+        $('.wow').each(function(i, el){
+
+          var elWatcher = scrollMonitor.create( $(el) );
+
+          var delay;
+          if ( $(window).width() < 768 ){
+            delay = 0
+          } else {
+            delay = $(el).data('animation-delay');
+          }
+
+          var animationClass
+
+          if ( $(el).data('animation-class') ){
+            animationClass = $(el).data('animation-class');
+          } else {
+            animationClass = "wowFadeUp"
+          }
+
+          var animationName
+
+          if ( $(el).data('animation-name') ){
+            animationName = $(el).data('animation-name');
+          } else {
+            animationName = "wowFade"
+          }
+
+          elWatcher.enterViewport(throttle(function() {
+            $(el).addClass(animationClass);
+            $(el).css({
+              'animation-name': animationName,
+              'animation-delay': delay,
+              'visibility': 'visible'
+            });
+          }, 100, {
+            'leading': true
+          }));
+          elWatcher.exitViewport(throttle(function() {
+            $(el).removeClass(animationClass);
+            $(el).css({
+              'animation-name': 'none',
+              'animation-delay': 0,
+              'visibility': 'hidden'
+            });
+          }, 100));
+        });
       }
-    });
 
-    $(this).parent().toggleClass('active');
-  });
+    },300);
+  }
 
-  $('.ui-select__dropdown span').on('click', function(){
-    // parse value and toggle active
-    var value = $(this).data('val');
-    if (value){
-      $(this).siblings().removeClass('active');
-      $(this).addClass('active');
-
-      // set visible
-      $(this).closest('.ui-select').removeClass('active');
-      $(this).closest('.ui-select').find('input').val(value);
-
-      $(this).closest('.ui-select').find('.ui-select__visible span').text(value);
-    }
-
-  });
-
-  // handle outside click
-  $(document).click(function (e) {
-    var container = new Array();
-    container.push($('.ui-select'));
-
-    $.each(container, function(key, value) {
-        if (!$(value).is(e.target) && $(value).has(e.target).length === 0) {
-            $(value).removeClass('active');
-        }
-    });
-  });
-
-  // numeric input
-  $('.ui-number span').on('click', function(e){
-    var element = $(this).parent().find('input');
-    var currentValue = parseInt($(this).parent().find('input').val()) || 0;
-
-    if( $(this).data('action') == 'minus' ){
-      if(currentValue <= 1){
-        return false;
-      }else{
-        element.val( currentValue - 1 );
-      }
-    } else if( $(this).data('action') == 'plus' ){
-      if(currentValue >= 99){
-        return false;
-      } else{
-        element.val( currentValue + 1 );
-      }
-    }
-  });
-
-  // Masked input
-  $(".js-dateMask").mask("99.99.99",{placeholder:"ДД.ММ.ГГ"});
-  $("input[type='tel']").mask("+7 (000) 000-0000", {placeholder: "+7 (___) ___-____"});
+  runScrollMonitor();
 
 });
+
+
+// YANDEX MAPS
+ymaps.ready(init);
+var myMap,myPlacemark;
+
+function init(){
+  myMap = new ymaps.Map("map", {
+      center: [51.760100, 55.099884],
+      zoom: 17
+  });
+
+  myPlacemark = new ymaps.Placemark([51.760100, 55.099884], {
+      hintContent: 'Choppers',
+      balloonContent: 'улица 9 Января, 28'
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: '../img/placemark.png',
+    iconImageSize: [40, 48],
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+}
